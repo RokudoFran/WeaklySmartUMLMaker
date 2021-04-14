63957ca836bd865a3295aec8983cb4eba458a4a6
UserJul <yuliya.selezneva.1999@gmail.com>
2021-04-14 16:39:10 +0300

    restruct patern

---
+++ WeaklySmartUMLMaker/Fabrics/AggregationPolygonFabric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaklySmartUMLMaker.Fabrics
+{
+    public class AggregationPolygonFabric : FigureFabric
+    {
+        protected override AbstructFigure CreateFigure()
+        {
+            return new AggregationPolygon(Holst.Instanse.Pen, Holst.Instanse.Graphics);
+        }
+    }
+}
